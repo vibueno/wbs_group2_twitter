d1b7de7f1ffd547ca521058e74d65cf94eb7be4e
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-26 13:08:40 +0100

    Add user controller functions
    - getAll
    - GetbyId

--- controllers/users.js
+++ controllers/users.js
@@ -9,12 +9,82 @@ const {
   resOpFailure
 } = require('../vars/constants');
 
+const {
+  msgFetchedAllUsers,
+  msgDBError,
+  msgInvalidIdFormat
+} = require('../vars/messages');
+
+const { validId } = require('../utils/validations');
+
+const sqlAllUsers = 'SELECT * FROM users';
+
 const usersController = {
   getAll: async (req, res) => {
-    res.status(httpOK).send('I am all users');
+    try {
+      const users = await pool.query(sqlAllUsers);
+
+      res
+        .status(httpOK)
+        .json(
+          buildResponse(httpOK, resOpSuccess, msgFetchedAllUsers, users.rows)
+        );
+    } catch (e) {
+      console.error(Error(e.message));
+      if (e.status) res.status(e.status).json(e);
+      else {
+        res
+          .status(httpServerError)
+          .json(
+            buildResponse(httpServerError, resOpFailure, msgDBError, e.message)
+          );
+      }
+    }
   },
   getById: async (req, res) => {
-    res.status(httpOK).send('I am user by id');
+    const { id } = req.params;
+
+    try {
+      if (!validId(id)) {
+        throw buildResponse(httpBadRequest, resOpFailure, msgInvalidIdFormat);
+      }
+
+      const sqlUserById = {
+        text: sqlAllUsers + ' WHERE id=$1;',
+        values: [parseInt(id)]
+      };
+
+      const user = await pool.query(sqlUserById);
+
+      if (!user.rows.length) {
+        throw buildResponse(
+          httpNotFound,
+          resOpSuccess,
+          `User with id ${id} does not exist`
+        );
+      }
+
+      res
+        .status(httpOK)
+        .json(
+          buildResponse(
+            httpOK,
+            resOpSuccess,
+            `Succesfully fetched user with id ${id}`,
+            user.rows[0]
+          )
+        );
+    } catch (e) {
+      console.error(Error(e.message));
+      if (e.status) res.status(e.status).json(e);
+      else {
+        res
+          .status(httpServerError)
+          .json(
+            buildResponse(httpServerError, resOpFailure, msgDBError, e.message)
+          );
+      }
+    }
   },
   getMessagesByUserId: async (req, res) => {
     res.status(httpOK).send('I am messages by user id');
--- vars/messages.js
+++ vars/messages.js
@@ -1,5 +1,9 @@
 const messages = {
-  msgServerStarted: 'Server started'
+  msgServerStarted: 'Server started',
+  msgFetchedAllUsers: 'Succesfully fetched all users',
+  msgDBError: 'Internal DB error',
+  msgInvalidIdFormat:
+    'Incorrect id format. Only positive integers greater than 0 are valid ids'
 };
 
 module.exports = messages;
