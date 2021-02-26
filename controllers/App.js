1063e189bd2d00523fa838fda3c0063c88546c3a
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-26 10:51:08 +0100

    Create project structure & config

---
+++ controllers/App.js
@@ -0,0 +1,14 @@
+const { httpNotFound, resOpFailure } = require('../variables/constants');
+const { msgPageNotFound } = require('../variables/messages');
+
+const buildResponse = require('../utils/response');
+
+const appController = {
+  pageNotFound: (req, res) => {
+    res
+      .status(httpNotFound)
+      .json(buildResponse(httpNotFound, resOpFailure, msgPageNotFound));
+  }
+};
+
+module.exports = appController;
---
+++ vars/constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  httpOK: 200,
+  httpBadRequest: 400,
+  httpNotFound: 404,
+  httpServerError: 500,
+  resOpSuccess: 'succeeded',
+  resOpFailure: 'failed',
+
+  validInfoRequests: ['type', 'name', 'base'],
+  hallOfFameDefaultLimit: 10
+};
+
+module.exports = constants;
